9d087de8ee057383149e775ce4f71f13386431c4
Dongja <jdh981118@naver.com>
2023-08-03 14:16:01 +0900

    chore: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve:{
+    resolve: {
+      alias: [
+        {find:'@',replacement: resolve(__dirname, './src')},
+    ]
+    },
+  },
+  plugins: [vue(), tsconfigPaths()],
 })
