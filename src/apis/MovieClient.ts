83e71f8567fb4188122c0d77ffdea7f94e6f615e
Dongja <jdh981118@naver.com>
2023-08-06 19:19:56 +0900

    feat: api class 추가

---
+++ src/apis/MovieClient.ts
@@ -0,0 +1,19 @@
+import axios,{Axios}from 'axios'
+
+const API_END_POINT = 'https://omdbapi.com'
+
+export default class MovieClient{
+  private httpClient: Axios
+  constructor(){
+    this.httpClient = axios.create({
+      baseURL:API_END_POINT,
+      params:{
+        apikey:import.meta.env.VITE_API_KEY
+      }
+    })
+  }
+
+  async getData(params) {
+    return this.httpClient.get('', params);
+  }
+}
