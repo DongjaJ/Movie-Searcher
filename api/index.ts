a2f570285f0116a2da14a0b8bcce4dd9d12ea075
Dongja <jdh981118@naver.com>
2023-08-15 15:34:40 +0900

    delete: serverless 함수에서 interface 제거

--- api/index.ts
+++ api/index.ts
@@ -1,35 +1,4 @@
 import axios, { Axios } from 'axios';
-interface IMovieDetailResponse {
-  Title: string;
-  Year: string;
-  Rated: string;
-  Released: string;
-  Runtime: string;
-  Genre: string;
-  Director: string;
-  Writer: string;
-  Actors: string;
-  Plot: string;
-  Language: string;
-  Country: string;
-  Awards: string;
-  Poster: string;
-  Ratings: {
-    Source: string;
-    Value: string;
-  }[];
-  Metascore: string;
-  imdbRating: string;
-  imdbVotes: string;
-  imdbID: string;
-  Type: string;
-  DVD: string;
-  BoxOffice: string;
-  Production: string;
-  Website: string;
-  Response: string;
-}
-
 interface SearchParams {
   keyword: string;
   releaseYear?: number;
@@ -56,11 +25,7 @@ class MovieClient {
     return response.data;
   }
 
-  async getMovieDetail({
-    movieId,
-  }: {
-    movieId: string;
-  }): Promise<IMovieDetailResponse> {
+  async getMovieDetail({ movieId }: { movieId: string }) {
     const response = await this.httpClient.get('', {
       params: { i: movieId, plot: 'full' },
     });
